2f8699b05586044f042d0c726384cc78518bae78
yokoyas000 <yokoyama.s.1235@gmail.com>
2018-05-13 23:46:26 +0900

    プロジェクト作成

---
+++ TDDExample/TDDExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TDDExample
+//
+//  Created by yokoyas000 on 2018/05/07.
+//  Copyright © 2018 yokoyas000. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
