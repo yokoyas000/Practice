d107b5bc510339ca33291931f665e449d0722179
yokoyas000 <yokoyama.s.1235@gmail.com>
2018-05-21 21:50:15 +0900

    create project

---
+++ PromiseKit/PromiseKit/PromiseKit.h
@@ -0,0 +1,19 @@
+//
+//  PromiseKit.h
+//  PromiseKit
+//
+//  Created by saki on 2018/05/21.
+//  Copyright © 2018年 yokoyas000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromiseKit.
+FOUNDATION_EXPORT double PromiseKitVersionNumber;
+
+//! Project version string for PromiseKit.
+FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseKit/PublicHeader.h>
+
+
