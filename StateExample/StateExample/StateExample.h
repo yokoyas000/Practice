b83c4230dda70deee9f4a31c88a727f0f9cb025a
yokoyas000 <yokoyama.s.1235@gmail.com>
2018-05-27 06:56:42 +0900

    create project

---
+++ StateExample/StateExample/StateExample.h
@@ -0,0 +1,19 @@
+//
+//  StateExample.h
+//  StateExample
+//
+//  Created by saki on 2018/05/27.
+//  Copyright © 2018年 yokoyas000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateExample.
+FOUNDATION_EXPORT double StateExampleVersionNumber;
+
+//! Project version string for StateExample.
+FOUNDATION_EXPORT const unsigned char StateExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateExample/PublicHeader.h>
+
+
